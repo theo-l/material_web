b36a392681df95a2040d178d4a3d37f0e2b301e0
theo-l <theol.liang@gamil.com>
2016-10-23 23:56:13 -0200

    push by shutdown

--- material/tests.py
+++ material/tests.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-  
 import datetime
 from django.test import TestCase
 
@@ -107,6 +108,85 @@ class QuerySetMethodTest(TestCase):
         self.assertSequenceEqual(OutMaterial.objects.exclude(material__name__iexact='fibra'),[])
 
 
+    def test_order_by(self):
+        '手动指定 QuerySet 的排序方式'
+        # 按照创建时间升序排列
+        self.assertSequenceEqual(InMaterial.objects.all().order_by('create_time'),[self.in_material,self.in_material2])
+
+        '通过关联对象来指定当前model的排序方式'
+        #Join material
+        self.assertSequenceEqual(InMaterial.objects.order_by('material__id'), [self.in_material, self.in_material2])
+
+        #Not join material
+        self.assertSequenceEqual(InMaterial.objects.order_by('material_id'), [self.in_material, self.in_material2])
+
+        # 按照创建时间升序排列
+        self.assertSequenceEqual(OutMaterial.objects.all().order_by('create_time'),[self.out_material, self.out_material2])
+
+    def test_reverse(self):
+        self.assertSequenceEqual(InMaterial.objects.all().reverse(),[self.in_material, self.in_material2])
+        self.assertSequenceEqual(OutMaterial.objects.all().reverse(),[self.out_material, self.out_material2])
+        self.assertSequenceEqual(Material.objects.all().reverse(),[self.material2, self.material])
+
+    def test_distinct(self):
+        # distinct operation not supported by all database backend
+        #self.assertSequenceEqual(Material.objects.all().distinct('name'),[self.material])
+        pass
+        
+    def test_values(self):
+
+        self.assertSequenceEqual(
+                Material.objects.filter(type_no='24').values('id','name','type_no'),
+                [{
+                     'id':2,'name':'Fibra','type_no':'24' 
+                 }]
+                )
+
+    def test_values_list(self):
+
+        self.assertSequenceEqual(
+               Material.objects.filter(type_no='24').values_list('id','name','type_no') ,
+               [(2,'Fibra','24')]
+                )
+        
+    def test_dates(self):
+        #测试 dates()/datetimes() 具有相同的效果
+        # datetimes具有一些额外的类型: hour, minute, second
+        import datetime
+
+        self.assertSequenceEqual(
+               Material.objects.dates('create_time','year') ,
+               [datetime.date(2016,1,1) ]
+                )
+        self.assertSequenceEqual(
+               Material.objects.dates('create_time','month'),
+               [datetime.date(2016,10,1)]
+                )
+
+        self.assertSequenceEqual(
+               Material.objects.dates('create_time','day'), 
+               [datetime.date(2016,10,23)]
+                )
+        
+    def test_none(self):
+        
+        self.assertSequenceEqual(
+               Material.objects.none(),
+               []
+                )
+        
+    def test_select_related(self):
+        # 将与model对象关联的对象从数据库中加载到python中以便之后的访问不需要再进行数据库访问。
+        # select_related(*field)
+        pass
+        
+    def test_prefetch_related(self):
+        pass
+        
+        
+        
+        
+        
 
 
 
