172148d1dcbd3156df1993bad378db407ddc3005
theo-l <theol.liang@gamil.com>
2016-12-11 13:44:08 -0200

    push by shutdown

--- fabfile.py
+++ fabfile.py
@@ -1,9 +1,24 @@
 # encoding: utf-8
 from __future__ import with_statement
-from fabric.api import local, settings, abort, run, cd
+from fabric.api import local, settings, abort, run, cd, env
 from fabric.contrib.console import confirm
 
+def test_env_obj():
+    with settings(host_string="liang@dev"):
+        for (index, (key, value)) in enumerate(sorted(env.items())):
+            print("{}: {} = {}".format(index, key, value))
+
 # fabric 可以运行 fabfile.py 中定义的任意函数
+
+MOTO_HOST='admin@192.168.0.11'
+THEO_HOST='theo@192.168.0.190'
+env.hosts=[MOTO_HOST, THEO_HOST]
+
+def test_multi_host_taskA():
+    run('ls')
+    
+def test_multi_host_taskB():
+    run("whoami")
 
 # 使用 fab 调用带参数的方法时，其语法为 funcname:para_value
 def hello(name="world"):
@@ -35,3 +50,6 @@ def deploy():
     with cd(code_dir):
         run('git pull') # 运行操作系统中的命令, 需要指定主机进行连接
         run("ls") # 运行操作系统中的命令, 需要指定主机进行连接
+
+if __name__ == '__main__':
+    test_env_obj()
