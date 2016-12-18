1cbb2782545e1fcfb5ad54b84eadc3553f9bdb00
theo-l <theol.liang@gamil.com>
2016-12-18 14:15:48 -0200

    Default updating

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,5 @@
 # encoding: utf-8
+
 from __future__ import with_statement
 from fabric.api import local, settings, abort, run, cd, env, hosts, execute, roles
 from fabric.contrib.console import confirm
